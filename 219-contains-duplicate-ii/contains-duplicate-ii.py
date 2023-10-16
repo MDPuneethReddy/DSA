b4701c7d8bb4e368a04d4c75d0749c6b33eb255b
MDPuneethReddy <64721445+MDPuneethReddy@users.noreply.github.com>
2023-10-16 13:44:50 -0400

    Time: 510 ms (83.96%) | Memory: 29.5 MB (58.32%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.py
+++ 219-contains-duplicate-ii/contains-duplicate-ii.py
@@ -1,5 +1,8 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        # time complexity - O(n)
+        # space complexity - O(n)
+        # save the values in dictionary and check get the difference between the present and already saved index else just update the dictionary
         d={}
         for i in range(len(nums)):
             if nums[i] not in d:
