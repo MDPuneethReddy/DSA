3a80930cb1da9a51d5c7121e7d19cd0d49cae592
MDPuneethReddy <64721445+MDPuneethReddy@users.noreply.github.com>
2023-11-05 11:56:56 -0500

    Time: 2132 ms (5.24%) | Memory: 311.7 MB (27.93%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
@@ -1,6 +1,8 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
         d={}
+        if edges==[]:
+            return True
         for i in range(len(edges)):
             if edges[i][0] in d:
                 d[edges[i][0]].append(edges[i][1])
@@ -14,53 +16,18 @@ class Solution:
                     d[edges[i][1]].append(edges[i][0])
                 else:
                     d[edges[i][1]]=[edges[i][0]]
-                
-        
-        # print(d)
-        def dfs(d,source,destination,boolean=False,visited=[0]*n):
-            # print("bbol",d,source,destination)
-            if (source==destination):
-                return True
-            if boolean==True:
-                visited=[1]*n
-            if visited[source]==1:
-                return False
-            else:
-                
-                visited[source]=1
-                for i in range(len(d[source])):
-                    boolean=boolean or dfs(d,d[source][i],destination,boolean,visited)
-            return boolean
-        return dfs(d,source,destination)
-        
-        # d={}
-        # if edges==[]:
-        #     return True
-        # for i in range(len(edges)):
-        #     if edges[i][0] in d:
-        #         d[edges[i][0]].append(edges[i][1])
-        #         if edges[i][1] in d:
-        #             d[edges[i][1]].append(edges[i][0])
-        #         else:
-        #             d[edges[i][1]]=[edges[i][0]]
-        #     else:
-        #         d[edges[i][0]]=[edges[i][1]]
-        #         if edges[i][1] in d:
-        #             d[edges[i][1]].append(edges[i][0])
-        #         else:
-        #             d[edges[i][1]]=[edges[i][0]]
-        # self.bool=False
-        # self.visited=[]
-        # def recurr(d,source,destination):
-        #     if source not in d:
-        #         return 
-        #     if source in self.visited:
-        #         return
-        #     self.visited.append(source)
-        #     if source==destination:
-        #         self.bool=True
-        #     for i in d[source]:
-        #         recurr(d,i,destination)
-        #     return
-        # recurr(d,source,destination)
-        # return self.bool        
+        self.bool=False
+        self.visited=[0]*n
+        def recurr(d,source,destination):
+            if source not in d:
+                return 
+            if self.visited[source]==1:
+                return
+            self.visited[source]=1
+            if source==destination:
+                self.bool=True
+            for i in d[source]:
+                recurr(d,i,destination)
+            return
+        recurr(d,source,destination)
+        return self.bool        
