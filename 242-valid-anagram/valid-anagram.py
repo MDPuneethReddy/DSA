096223f7901c3fbcf54e90e43cc8745307c36bda
MDPuneethReddy <64721445+MDPuneethReddy@users.noreply.github.com>
2023-12-26 22:55:04 -0500

    Time: 36 ms (98.71%) | Memory: 17.5 MB (18.42%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # space- o(n), time- o(n)
+        if len(s)!=len(t):
+            return False
+        d1={}
+        d2={}
+        for i in s:
+            if i in d1:
+                d1[i]+=1
+            else:
+                d1[i]=1
+        for i in t:
+            if i in d2:
+                d2[i]+=1
+            else:
+                d2[i]=1
+        for i in d2:
+            if i not in d1:
+                return False
+            if d1[i]!=d2[i]:
+                return False
+        return True
+        
+        
