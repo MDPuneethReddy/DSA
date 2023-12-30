eeb1aa1aff5146459b1e5cf24bf86e93b539cf41
MDPuneethReddy <64721445+MDPuneethReddy@users.noreply.github.com>
2023-12-29 23:27:04 -0500

    Time: 258 ms (5.02%) | Memory: 23.1 MB (5.40%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,21 +1,23 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        #learn what is meant by bucket sort
         d={}
         for i in nums:
             if i not in d:
                 d[i]=1
             else:
                 d[i]+=1
+        l = [[] for _ in range(len(nums))]
+        for i in d:
+            l[d[i]-1].append(i)
         values=[]
-        for i in range(k):
-            maxi=-math.inf
-            key=0
-            for i in d:
-                if d[i]>=maxi:
-                    maxi=max(maxi,d[i])
-                    key=i
-            values.append(key)
-            del d[key]
-
-        return values
+        for i in range(len(nums)):
+            if(nums[len(nums)-i-1]!=[]):
+                for j in l[len(nums)-i-1]:
+                    values.append(j)
+                    k-=1
+                    print(values,k)
+                    if(k<=0):
+                        return values
+        
         
