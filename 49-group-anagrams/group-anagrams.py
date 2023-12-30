1152efb384bff1385ad81d8c0cf09e9daacc0cd8
MDPuneethReddy <64721445+MDPuneethReddy@users.noreply.github.com>
2023-12-30 12:42:53 -0500

    Time: 159 ms (5.31%) | Memory: 23 MB (5.86%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,15 +1,26 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # space-o(n) time- o(nlogn)
+        # d={}
+        # sortedArray=[]
+        # for i in strs:
+        #     sortedArray.append("".join(sorted(i)))
+        # # print(sortedArray,strs)
+        # for i in range(len(sortedArray)):
+        #     if sortedArray[i] in d:
+        #         d[sortedArray[i]].append(strs[i])
+        #     else:
+        #         d[sortedArray[i]]=[strs[i]]
+        # # print(d)
+        # return d.values()
         d={}
-        sortedArray=[]
         for i in strs:
-            sortedArray.append("".join(sorted(i)))
-        # print(sortedArray,strs)
-        for i in range(len(sortedArray)):
-            if sortedArray[i] in d:
-                d[sortedArray[i]].append(strs[i])
+            c=[0]*26
+            for j in i:
+                c[ord(j)-ord("a")]+=1
+            if tuple(c) not in d:
+                d[tuple(c)]=[i]
             else:
-                d[sortedArray[i]]=[strs[i]]
-        # print(d)
+                d[tuple(c)].append(i)
         return d.values()
         
