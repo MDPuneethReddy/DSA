9fbac59f6f4b4827abb166938cfd7bff1402fd11
MDPuneethReddy <64721445+MDPuneethReddy@users.noreply.github.com>
2024-01-21 22:59:24 -0500

    Time: 44 ms (38.53%) | Memory: 17.1 MB (58.19%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.py
@@ -0,0 +1,23 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        l=[]
+        def recurssion(open,close,s):
+            print(open,close,s)
+            if open==0 and close==0:
+                # print(open,close,s)
+                l.append(s)
+                return
+            if(open<close):
+                if(open>0):
+                    recurssion(open-1,close,s+"(") 
+                if(close>0):
+                    recurssion(open,close-1,s+")")
+            if(open==close):
+                recurssion(open-1,close,s+"(") 
+            return
+        recurssion(n,n,"")
+        return l
+            
+            
+            
+        
