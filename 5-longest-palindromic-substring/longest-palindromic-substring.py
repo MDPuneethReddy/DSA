6ad2621a41d93214899bbb3c274ae8f81a7516be
MDPuneethReddy <64721445+MDPuneethReddy@users.noreply.github.com>
2024-03-12 20:45:23 -0400

    Time: 370 ms (54.31%) | Memory: 16.5 MB (86.56%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,24 +1,50 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         def isPalindrome(string):
-            if(string==string[::-1]):
-                return True
-            else:
-                return False
+            # if string=="bb":
+            #     print(string)
+            start=0
+            end=len(string)-1
+            while(start<end):
+                # if string=="bb":
+                #     print(start,end,s[start],s[end])
+                if string[start]==string[end]:
+                    start+=1
+                    end-=1
+                else:
+                    return False
+            return True
+            # if(string==string[::-1]):
+            #     return True
+            # else:
+            #     return False
         c=0
         value=""
-        if len(s)==0:
-            return ""
-        if len(s)==1:
-            return s
         for i in range(len(s)):
-            for j in range(i+1,len(s)+1):
-                if(isPalindrome(s[i:j])):
-                    # print(s[i:j])
-                    if(len(s[i:j])>c):
-                        c=len(s[i:j])
-                        value=s[i:j]
+            start=i
+            end=i
+            while(start>=0 and end<=len(s)-1 and s[start]==s[end]):
+                if((end-start+1)>c):
+                    value=s[start:end+1]
+                    c=end-start+1
+                start-=1
+                end+=1
+
+            start=i
+            end=i+1
+            while(start>=0 and end<=len(s)-1 and s[start]==s[end]):
+                if((end-start+1)>c):
+                    value=s[start:end+1]
+                    c=end-start+1
+                start-=1
+                end+=1
+
         return value
+
+        # babad
+        # i=0, 
+        # start=0, end=0
+
 
 
 
