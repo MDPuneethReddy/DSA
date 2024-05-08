47c891ac895e4760e63dee4961d230f8ccf844a6
MDPuneethReddy <64721445+MDPuneethReddy@users.noreply.github.com>
2024-05-07 21:24:08 -0400

    Time: 49 ms (95.63%) | Memory: 17.9 MB (9.45%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,10 +1,13 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # space-o(n) time- o(n)
         d={}
         for i in range(len(nums)):
-            if nums[i] not in d:
-                d[target-nums[i]]=i
+           
+            
+            if nums[i] in d:
+                return [d[nums[i]],i]
             else:
-                return [i,d[nums[i]]]
+                v=target-nums[i]
+                d[target-nums[i]]=i
+        
         
