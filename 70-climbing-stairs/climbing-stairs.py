f3888510d271acd10499d49649956b584592f239
MDPuneethReddy <64721445+MDPuneethReddy@users.noreply.github.com>
2024-06-21 23:21:13 -0400

    Time: 35 ms (52.40%) | Memory: 16.4 MB (65.21%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,26 +1,12 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        self.dic={0:1,1:1}
-        def dp(n):
-            if n in self.dic:
-                return self.dic[n]
-            leftValue=0
-            rightValue=0
-            if n-1 in self.dic:
-                leftValue=self.dic[n-1]
-                
-            else:
-                leftValue=dp(n-1)
-                self.dic[n-1]=leftValue
-            if n-2  in self.dic:
-                rightValue=self.dic[n-2]
-                
-            else:
-                rightValue=dp(n-2)
-                self.dic[n-2]=rightValue
-            return leftValue+rightValue
-            
-        return dp(n)
-            
-        
+        def recurr(n,dp):
+            if n<=1:
+                return 1
+            if n in dp:
+                return dp[n]
+            value= recurr(n-1,dp)+recurr(n-2,dp)
+            dp[n]=value
+            return value
+        return recurr(n,{})
         
