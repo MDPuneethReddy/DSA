658eea21ffc95cb6957f323b0476433e6b554c29
MDPuneethReddy <64721445+MDPuneethReddy@users.noreply.github.com>
2024-06-21 23:24:48 -0400

    Time: 42 ms (9.85%) | Memory: 16.4 MB (93.95%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,12 +1,21 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        def recurr(n,dp):
-            if n<=1:
-                return 1
-            if n in dp:
-                return dp[n]
-            value= recurr(n-1,dp)+recurr(n-2,dp)
-            dp[n]=value
-            return value
-        return recurr(n,{})
+        # time-o(n) space-o(n)+o(n).   0(n) for recursion stack
+        # def recurr(n,dp):
+        #     if n<=1:
+        #         return 1
+        #     if n in dp:
+        #         return dp[n]
+        #     value= recurr(n-1,dp)+recurr(n-2,dp)
+        #     dp[n]=value
+        #     return value
+        # return recurr(n,{})
+
+        prev1=1
+        prev2=1
+        for i in range(2,n+1):
+            curr=prev1+prev2
+            prev2=prev1
+            prev1=curr
+        return prev1
         
