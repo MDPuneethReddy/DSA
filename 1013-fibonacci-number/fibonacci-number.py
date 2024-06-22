ae5e81b2e0a9a87d67032139aef4118334834771
MDPuneethReddy <64721445+MDPuneethReddy@users.noreply.github.com>
2024-06-21 22:59:41 -0400

    Time: 34 ms (74.48%) | Memory: 16.4 MB (63.36%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,14 +1,14 @@
 class Solution:
     def fib(self, n: int) -> int:
-        dp={}
-        def recurr(n):
+        # dp={}
+        def recurr(n,dp):
             if n<=1:
                 return n
             if n in dp:
                 return dp[n]
-            value= recurr(n-1)+recurr(n-2)
+            value= recurr(n-1,dp)+recurr(n-2,dp)
             dp[n]=value
             return value
-        return recurr(n)
+        return recurr(n,{})
             
         
