39a7706744c9005cdfa9205ad0e28c439248785a
MDPuneethReddy <64721445+MDPuneethReddy@users.noreply.github.com>
2024-07-20 17:15:57 -0400

    Time: 44 ms (78.01%) | Memory: 19.4 MB (24.24%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.py
@@ -6,17 +6,16 @@
 #         self.right = right
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        self.maxDiamter=0
-        def recurrsion(root):
+        maxi=[0]
+        def recurr(root):
             if root is None:
                 return 0
-            leftVal=recurrsion(root.left)
-            rightVal=recurrsion(root.right)
-            height=1+max(leftVal,rightVal)
-            diameter=1+leftVal+rightVal
-            self.maxDiamter=max(self.maxDiamter,diameter)
-            # print(self.maxDiamter,diameter)
-            return height
-        recurrsion(root)
-        return self.maxDiamter-1
+            left=recurr(root.left)
+            right=recurr(root.right)
+            maxi[0]=max(maxi[0],left+right)
+            return 1+max(left,right)
+        recurr(root)
+        return maxi[0]
+        
+        
         
