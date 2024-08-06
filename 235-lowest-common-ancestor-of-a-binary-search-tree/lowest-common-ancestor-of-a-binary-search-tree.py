e32233218017639842e233e6146e3ad7bc21c4ec
MDPuneethReddy <64721445+MDPuneethReddy@users.noreply.github.com>
2024-08-06 09:01:19 -0400

    Time: 48 ms (89.09%) | Memory: 20.4 MB (24.56%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,27 +7,11 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        value=[]
-        def recurr(root,p,q):
-            if root is None:
-                return [False,False]
-            left=recurr(root.left,p,q)
-            right=recurr(root.right,p,q)
-            value1=False
-            value2=False
-            if root.val==p:
-                value1=True
-            if root.val==q:
-                value2=True
-            if left[0]==True or right[0]==True:
-                value1=True
-            if left[1]==True or right[1]==True:
-                value2=True
-            if value1==True and value2==True and len(value)==0:
-                value.append(root.val)
-            # print(value1,value2,root.val)
-            return [value1,value2]
-        recurr(root,p.val,q.val)
-        return TreeNode(value[0])
-
-        
+        curr=root
+        while(curr):
+            if curr.val>p.val and curr.val >q.val:
+                curr=curr.left
+            elif curr.val<p.val and curr.val <q.val:
+                curr=curr.right
+            else:
+                return curr
