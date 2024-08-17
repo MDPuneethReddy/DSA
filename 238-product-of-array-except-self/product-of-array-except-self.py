97618aa9bfa00c56e62b0e87a416942e2b892c9d
MDPuneethReddy <64721445+MDPuneethReddy@users.noreply.github.com>
2024-08-16 20:30:10 -0400

    Time: 295 ms (11.55%) | Memory: 26.3 MB (20.24%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,30 +1,57 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        c=0
-        for i in nums:
-            if i==0:
-                c+=1
-        if c>1:
-            return [0]*len(nums)
-        elif c==1:
-            product=1
-            for i in nums:
-                if i!=0:
-                    product*=i
-            l=[]
-            for i in nums:
-                if i!=0:
-                    l.append(0)
-                else:
-                    l.append(product)
-            return l
-        else:
+        # #with division
+        # c=0
+        # for i in nums:
+        #     if i==0:
+        #         c+=1
+        # if c>1:
+        #     return [0]*len(nums)
+        # elif c==1:
+        #     product=1
+        #     for i in nums:
+        #         if i!=0:
+        #             product*=i
+        #     l=[]
+        #     for i in nums:
+        #         if i!=0:
+        #             l.append(0)
+        #         else:
+        #             l.append(product)
+        #     return l
+        # else:
 
-            product=1
-            for i in nums:
-                product*=i
-            l=[]
-            for i in nums:
-                l.append(product//i)
-            return l
+        #     product=1
+        #     for i in nums:
+        #         product*=i
+        #     l=[]
+        #     for i in nums:
+        #         l.append(product//i)
+        #     return l
+        
+
+
+        #without division
+        left=[]
+        right=[0]*len(nums)
+        lp=1
+        for i in nums:
+            lp*=i
+            left.append(lp)
+        rp=1
+        for i in range(len(nums)):
+            rp*=nums[len(nums)-i-1]
+            right[len(nums)-i-1]=rp
+        # print(left,right)
+        l=[]
+        for i in range(len(nums)):
+            if i==0:
+                l.append(right[1])
+            elif i==len(nums)-1:
+                l.append(left[len(nums)-2])
+            else:
+                l.append(left[i-1]*right[i+1])
+        return l
+
+
         
