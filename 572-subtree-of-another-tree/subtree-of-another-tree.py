b2faed76df5b28ee383501256ef152d769fcc9c7
MDPuneethReddy <64721445+MDPuneethReddy@users.noreply.github.com>
2024-09-23 10:40:21 -0400

    Time: 82 ms (36.42%) | Memory: 16.7 MB (54.12%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/subtree-of-another-tree.py
@@ -6,26 +6,25 @@
 #         self.right = right
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        self.boolean=False
-        def checkEqual(root,subRoot):
+        def checkequal(root1,root2):
+            if root1 is None and root2 is None:
+                return True
+            if root1 is None or root2 is None:
+                return False
+            if root1.val != root2.val:
+                return False
+            return checkequal(root1.left,root2.left) and checkequal(root1.right,root2.right)
+        
+        self.value=False
+        def recurr(root,subRoot):
             if root is None and subRoot is None:
                 return True
-            if root is not None and subRoot is None:
-                return False
-            if root is None and subRoot is not None:
-                return False
-            if root.val!=subRoot.val:
-                return False
-            return checkEqual(root.left,subRoot.left) and checkEqual(root.right,subRoot.right)
-        def recurrsion(root,subRoot):
-            if root is None:
-                return True
-            if root.val==subRoot.val:
-                self.boolean=self.boolean or checkEqual(root,subRoot)
-            recurrsion(root.left,subRoot)
-            recurrsion(root.right,subRoot)
+            if root:
+                if root and subRoot and root.val==subRoot.val:
+                    self.value=self.value or checkequal(root,subRoot)
+                recurr(root.left,subRoot)
+                recurr(root.right,subRoot)
             return
-        recurrsion(root,subRoot)
-        return self.boolean
-            
+        recurr(root,subRoot)
+        return self.value
         
