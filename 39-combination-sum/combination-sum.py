1e416abfe9b1bb54c25082beb525d9405c90e88c
MDPuneethReddy <64721445+MDPuneethReddy@users.noreply.github.com>
2024-09-28 00:02:07 -0400

    Time: 47 ms (82.24%) | Memory: 16.7 MB (12.64%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,25 +1,25 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        mainList=set()
+        mainList=[]
         def recurr(candidates,target,s,ind,l):
             # print(s,ind,l)
             if s>target:
                 return
             if s==target:
-                mainList.add(tuple(l[:]))
+                mainList.append(l[:])
                 return
             if ind>=len(candidates):
                 if s==target:
-                    mainList.add(tuple(l[:]))
+                    mainList.append(l[:])
                 return
             recurr(candidates,target,s,ind+1,l)
             l.append(candidates[ind])
             recurr(candidates,target,s+candidates[ind],ind,l)
-            recurr(candidates,target,s+candidates[ind],ind+1,l)
+            # recurr(candidates,target,s+candidates[ind],ind+1,l)
             l.pop()
             return
         recurr(candidates,target,0,0,[])
-        return list(mainList)
+        return mainList
         
             
             
