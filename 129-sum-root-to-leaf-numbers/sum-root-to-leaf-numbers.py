84846e50bd3b05f205fe34d76caa30401720d049
MDPuneethReddy <64721445+MDPuneethReddy@users.noreply.github.com>
2024-10-01 20:49:32 -0400

    Time: 34 ms (67.15%) | Memory: 16.6 MB (33.19%) - LeetSync

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.py
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.py
@@ -6,18 +6,17 @@
 #         self.right = right
 class Solution:
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
-        main=[]
+        total=[0]
         def recurr(root,s):
             if root is None:
                 return
             if root.left is None and root.right is None:
-                main.append(s+str(root.val))
+                s+=str(root.val)
+                total[0]+=int(s)
+                return
             recurr(root.left,s+str(root.val))
             recurr(root.right,s+str(root.val))
             return
         recurr(root,"")
-        ts=0
-        for i in main:
-            ts+=int(i)
-        return ts
+        return total[0]
         
