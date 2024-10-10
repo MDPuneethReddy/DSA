18c2ea296db2d1ab136a540a26b109be0afa6d6d
MDPuneethReddy <64721445+MDPuneethReddy@users.noreply.github.com>
2024-10-09 23:53:30 -0400

    Time: 58 ms (35.65%) | Memory: 19.1 MB (8.94%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -1,27 +1,25 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        l=len(cost)
-        cost.append(0)
-        mem={}
-        def recurr(cost,index,su):
-            if index in mem:
-                return mem[index]
-            if index<0:
-                return 0
-            if index>=0 and index<=1:
-                return cost[index]
-            su=cost[index]
-            left=recurr(cost,index-1,su)
-            right=recurr(cost,index-2,su)
-            su+=min(left,right)
-            mem[index]=su
-            return su
-        return recurr(cost,l,0)
-
-          
-
-        
-
-       
-            
+        init=len(cost)
+        d={0:cost[0],1:cost[1]}
+        def recurr(init,s):
+            if init==1:
+                return cost[1]
+            elif init==0:
+                return cost[0]
+            if init-1 in d:
+                v1=d[init-1]
+            else:
+                v1=recurr(init-1,s)
+                d[init-1]=v1
+            if init-2 in d:
+                v2=d[init-2]
+            else:
+                v2=recurr(init-2,s)
+                d[init-2]=v2
+            v= min(v1,v2)
+            if init<=len(cost)-1:
+                s+=cost[init]
+            return s+v
+        return recurr(init,0)
         
