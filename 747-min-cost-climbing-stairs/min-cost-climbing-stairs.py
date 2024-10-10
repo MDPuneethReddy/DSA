7a7316119b3c30f56a7dbcbcf3b1b90ec3c5b938
MDPuneethReddy <64721445+MDPuneethReddy@users.noreply.github.com>
2024-10-10 00:00:53 -0400

    Time: 60 ms (23.87%) | Memory: 16.8 MB (34.60%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -2,24 +2,9 @@ class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         init=len(cost)
         d={0:cost[0],1:cost[1]}
-        def recurr(init,s):
-            if init==1:
-                return cost[1]
-            elif init==0:
-                return cost[0]
-            if init-1 in d:
-                v1=d[init-1]
-            else:
-                v1=recurr(init-1,s)
-                d[init-1]=v1
-            if init-2 in d:
-                v2=d[init-2]
-            else:
-                v2=recurr(init-2,s)
-                d[init-2]=v2
-            v= min(v1,v2)
-            if init<=len(cost)-1:
-                s+=cost[init]
-            return s+v
-        return recurr(init,0)
-        
+        for i in range(2,len(cost)+1):
+            v=min(d[i-1],d[i-2])
+            if i<=len(cost)-1:
+                v+=cost[i]
+            d[i]=v
+        return d[init]
