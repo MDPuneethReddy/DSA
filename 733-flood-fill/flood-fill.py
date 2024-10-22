9f55320817b9a3b931d974ce2396433f8f8c8b90
MDPuneethReddy <64721445+MDPuneethReddy@users.noreply.github.com>
2024-10-22 16:56:27 -0400

    Time: 0 ms (100.00%) | Memory: 17.1 MB (11.16%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -1,14 +1,14 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        visited=[[0 for i in range(len(image[0]))] for _ in range(len(image))]
+        # visited=[[0 for i in range(len(image[0]))] for _ in range(len(image))]
         def dfs(i,j,initialColor):
             if i<0 or j<0 or i>=len(image) or j>=len(image[0]):
                 return
-            if visited[i][j]==1:
+            # if visited[i][j]==1:
+            #     return
+            if image[i][j]!=initialColor or image[i][j]==color:
                 return
-            if image[i][j]!=initialColor:
-                return
-            visited[i][j]=1
+            # visited[i][j]=1
             image[i][j]=color
             dfs(i-1,j,initialColor)
             dfs(i,j-1,initialColor)
