2948bf1a65f259cfb6289d260cd8a0d38ed3832f
MDPuneethReddy <64721445+MDPuneethReddy@users.noreply.github.com>
2024-10-23 11:59:05 -0400

    Time: 643 ms (75.01%) | Memory: 19.7 MB (97.12%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,26 +1,26 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        mainList=[]
         nums.sort()
+        mainList=[]
         for i in range(len(nums)):
-            if i>0 and nums[i]==nums[i-1]:
+            if i!=0 and nums[i]==nums[i-1]:
                 continue
-            x=nums[i]
-            y=i+1
-            z=len(nums)-1
-            while(y<z):
-                if x+nums[y]+nums[z]==0:
-                    mainList.append([x,nums[y],nums[z]])
-                    while y < z and nums[y] == nums[y + 1]:
-                        y += 1
-                    while y < z and nums[z] == nums[z - 1]:
-                        z -= 1
-                    y+=1
-                    z-=1
-                elif x+nums[y]+nums[z]<0:
-                    y+=1
-                else:
-                    z-=1
+            else:
+                y=i+1
+                z=len(nums)-1
+                while(y<z):
+                    cs=nums[i]+nums[y]+nums[z]
+                    if cs==0:
+                        mainList.append([nums[i],nums[y],nums[z]])
+                        while y<z and nums[y]==nums[y+1]:
+                            y+=1
+                        while y<z and nums[z]==nums[z-1]:
+                            z-=1
+                        y+=1
+                        z-=1
+                    elif cs<0:
+                        y+=1
+                    else:
+                        z-=1
         return mainList
-
         
