ea6ebfa617ef3be2d96c708c8e13dad24a4c2ae3
MDPuneethReddy <64721445+MDPuneethReddy@users.noreply.github.com>
2024-10-27 23:01:59 -0400

    Time: 4 ms (52.79%) | Memory: 18.9 MB (6.87%) - LeetSync

--- 530-minimum-absolute-difference-in-bst/minimum-absolute-difference-in-bst.py
+++ 530-minimum-absolute-difference-in-bst/minimum-absolute-difference-in-bst.py
@@ -6,36 +6,16 @@
 #         self.right = right
 class Solution:
     def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
-        # self.mini=inf
-        # def recurr(root):
-        #     if root is None:
-        #         return
-        #     if root.left:
-        #         self.mini=min(self.mini,abs(root.val-root.left.val))
-        #     if root.right:
-        #         self.mini=min(self.mini,abs(root.val-root.right.val))
-        #     recurr(root.left)
-        #     recurr(root.right)
-        #     return
-        # recurr(root)
-        # return self.mini
-        self.values=[]
+        self.prev=None
+        self.res=math.inf
         def recurr(root):
             if root is None:
-                return 
-            self.values.append(root.val)
+                return
             recurr(root.left)
+            if self.prev:
+                self.res=min(self.res,abs(root.val-self.prev.val))
+            self.prev=root
             recurr(root.right)
-            return
         recurr(root)
-        self.values.sort()
-        if self.values==[]:
-            return []
-        if len(self.values)==1:
-            return self.values[0]
-        else:
-            val=inf
-            for i in range(1,len(self.values)):
-                val=min(val,abs(self.values[i]-self.values[i-1]))
-            return val
         
+        return self.res
