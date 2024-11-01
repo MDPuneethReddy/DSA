7389f85f075654a2e2d8b7ee51e4c6c95ec5d6fd
MDPuneethReddy <64721445+MDPuneethReddy@users.noreply.github.com>
2024-11-01 15:05:40 -0400

    Time: 0 ms (100.00%) | Memory: 16.5 MB (62.48%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -1,26 +1,24 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        top=0
-        bottom=len(matrix)
         left=0
         right=len(matrix[0])
-        results=[]
-        while(top<bottom and left<right):
+        top=0
+        bottom=len(matrix)
+        values=[]
+        while(left<right and top<bottom):
             for i in range(left,right):
-                results.append(matrix[top][i])
+                values.append(matrix[top][i])
             top+=1
             for i in range(top,bottom):
-                results.append(matrix[i][right-1])
+                values.append(matrix[i][right-1])
             right-=1
-            if not (left<right and top<bottom):
-                break
-            for i in range(right-1,left-1,-1):
-                results.append(matrix[bottom-1][i])
-            bottom-=1
-            for i in range(bottom-1,top-1,-1):
-                results.append(matrix[i][left])
-            left+=1
-            
-        return results
+            if left<right and top<bottom:
+                for i in range(right-1,left-1,-1):
+                    values.append(matrix[bottom-1][i])
+                bottom-=1
+                for i in range(bottom-1,top-1,-1):
+                    values.append(matrix[i][left])
+                left+=1
+        return values
 
         
