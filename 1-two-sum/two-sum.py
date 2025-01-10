98b4ab0a90eb0188cca8881e6770dbfa3c0cc208
MDPuneethReddy <64721445+MDPuneethReddy@users.noreply.github.com>
2025-01-09 19:32:09 -0500

    Time: 3 ms (53.51%) | Memory: 19.2 MB (5.31%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,13 +1,13 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        #time complexity- 0(n), space complexity-o(n)
         d={}
         for i in range(len(nums)):
-           
-            
             if nums[i] in d:
-                return [d[nums[i]],i]
-            else:
-                v=target-nums[i]
-                d[target-nums[i]]=i
+                return [i,d[nums[i]]]
+            d[target-nums[i]]=i
         
+        #withoutspace complexity then we can sort it use left and right to find the solutiuon. the time is o(nlogn)
+
+
         
