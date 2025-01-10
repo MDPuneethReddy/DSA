15fede8c1efc6b2a3d4b4247ee93a6c9979e7675
MDPuneethReddy <64721445+MDPuneethReddy@users.noreply.github.com>
2025-01-09 20:53:16 -0500

    Time: 46 ms (15.20%) | Memory: 18.3 MB (5.38%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,33 +1,18 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        if len(s)==0:
-            return 0
-        if len(s)==1:
-            return 1
-        unique=set()
-        unique.add(s[0])
-        # values=[s[0]]
-        i=0
-        j=1
-        count=1
-        maxi=0
-        while(i<=j and j<len(s) and i<len(s)):
-            if s[j] in unique:
-                maxi=max(maxi,count)
-                while(1):
-                    unique.remove(s[i])
-                    if s[j] in unique:
-                        i+=1
-                    else:
-                        unique.add(s[j])
-                        i+=1
-                        count=j-i+1
-                        j+=1
-                        break 
-            else:
-                count+=1
-                unique.add(s[j])
-                # values.append(s[j])
-                j+=1
-        maxi=max(maxi,count)
-        return maxi
+        #sliding window 2 pointer 
+        d={}
+        left=0
+        right=0
+        output=0
+        while(right<len(s)):
+            # print(d)
+            if s[right] in d:
+                left=max(left,d[s[right]]+1)
+            d[s[right]]=right
+            output=max(output,right-left+1)
+            print(output)
+            right+=1
+        return output
+
+        
