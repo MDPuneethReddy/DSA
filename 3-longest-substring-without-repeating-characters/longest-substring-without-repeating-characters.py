493abe49858f96f37d5c9e880bb097077ec58302
MDPuneethReddy <64721445+MDPuneethReddy@users.noreply.github.com>
2025-01-09 20:53:49 -0500

    Time: 23 ms (32.24%) | Memory: 18 MB (5.38%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,6 +1,6 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        #sliding window 2 pointer 
+        #sliding window 2 pointer time complexity-o(n), space complexity- o(n)
         d={}
         left=0
         right=0
@@ -11,7 +11,7 @@ class Solution:
                 left=max(left,d[s[right]]+1)
             d[s[right]]=right
             output=max(output,right-left+1)
-            print(output)
+            # print(output)
             right+=1
         return output
 
