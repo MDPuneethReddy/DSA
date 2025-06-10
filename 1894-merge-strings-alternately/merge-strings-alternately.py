97f665d079d7faaa489ac563e07fded43173602d
MDPuneethReddy <64721445+MDPuneethReddy@users.noreply.github.com>
2025-06-10 17:47:15 -0400

    Time: 35 ms (75.30%) | Memory: 17.8 MB (34.02%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -5,20 +5,13 @@ class Solution:
         mergedString=""
         counter1=0
         counter2=0
-        oddOrEvenCounter=True
         while(counter1<len(word1) and counter2<len(word2)):
-            if oddOrEvenCounter:
-                mergedString+=word1[counter1]
-                counter1+=1
-                oddOrEvenCounter=False
-            else:
-                mergedString+=word2[counter2]
-                counter2+=1
-                oddOrEvenCounter=True
-        if(counter1<len(word1)):
-            mergedString+=word1[counter1:]
-        if(counter2<len(word2)):
-            mergedString+=word2[counter2:]
+            mergedString+=word1[counter1]
+            counter1+=1
+            mergedString+=word2[counter2]
+            counter2+=1
+        mergedString+=word1[counter1:]
+        mergedString+=word2[counter2:]
         return mergedString
 
 
