7a68cd8cda2d01e50323e4d4e559363c49d912e0
MDPuneethReddy <64721445+MDPuneethReddy@users.noreply.github.com>
2025-06-11 11:45:33 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (51.29%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,24 +1,35 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack=[]
-        for i in range(len(s)):
-            if s[i] in ["(","{","["]:
-                stack.append(s[i])
+        for i in s:
+            if i in ["(","{","["]:
+                stack.append(i)
             else:
-                if len(stack)==0:
-                    return False
-                v=stack.pop()
-                if v in ["}","]",")"]:
-                    return False
-                # print(v,s[i])
-                if v=="(" and s[i] in ["}","]"]:
-                    return False
-                if v=="{" and s[i] in [")","]"]:
-                    return False
-                if v=="[" and s[i] in ["}",")"]:
-                    return False
+                if i==")":
+                    if len(stack)<=0:
+                        return False
+                    else:
+                        if stack[-1]!="(":
+                            return False
+                        else:
+                            stack.pop()
+                elif i=="]":
+                    if len(stack)<=0:
+                        return False
+                    else:
+                        if stack[-1]!="[":
+                            return False
+                        else:
+                            stack.pop()
+                elif i=="}":
+                    if len(stack)<=0:
+                        return False
+                    else:
+                        if stack[-1]!="{":
+                            return False
+                        else:
+                            stack.pop()
         if len(stack)>0:
             return False
         return True
-                
         
