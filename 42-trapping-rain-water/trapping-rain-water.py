62a5ae17eb392b3e6eb12bfdcfe7163b5308e372
MDPuneethReddy <64721445+MDPuneethReddy@users.noreply.github.com>
2025-06-13 13:05:07 -0400

    Time: 35 ms (12.53%) | Memory: 19.4 MB (38.96%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,42 +1,20 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        #time complexity -o(n) and space complexity -o(n)
-        if len(height)==0:
-            return 0
-        leftmax=[]
-        rightmax=[0]*len(height)
-        lmax=0
-        rmax=0
+        maxleft=0
+        maxright=0
+        left=[]
+        right=[0]*len(height)
         for i in range(len(height)):
-            leftmax.append(lmax)
-            lmax=max(lmax,height[i])
-        for i in range(len(height)-1,-1,-1):
-            rightmax[i]=rmax
-            rmax=max(rmax,height[i])
-        count=0
+            left.append(maxleft)
+            maxleft=max(maxleft,height[i])
         for i in range(len(height)):
-                count += min(leftmax[i], rightmax[i]) - height[i] if min(leftmax[i], rightmax[i]) - height[i] >= 0 else 0
-
-        return count
-
-        #time complexity -o(n), space- o(1)
-        # if len(height)==0 :
-        #     return 0
-        # l,r=0,len(height)-1
-        # leftmax=height[l]
-        # rightmax=height[r]
-        # res=0
-        # while(l<r):
-        #     if leftmax<=rightmax:
-        #         l+=1
-        #         leftmax=max(leftmax,height[l])
-        #         if (leftmax-height[l]>0):
-        #             res+=leftmax-height[l]
-        #     else:
-        #         r-=1
-        #         rightmax=max(rightmax,height[r])
-        #         if (rightmax-height[r]>0):
-        #             res+=rightmax-height[r]
-        # return res
+            right[len(height)-i-1]=maxright
+            maxright=max(maxright,height[len(height)-i-1])
+        total=0
+        for i in range(len(height)):
+            val=min(left[i],right[i])-height[i]
+            if val>0:
+                total+=val
+        return total
 
         
