6bf2fc946c5daf90d9549b6cc8ad3b3f980a1dab
MDPuneethReddy <64721445+MDPuneethReddy@users.noreply.github.com>
2025-06-13 11:54:29 -0400

    Time: 270 ms (31.49%) | Memory: 29 MB (6.05%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,24 +1,29 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        visited=[[0 for i in range(len(grid[0]))] for _ in range(len(grid))]
-        # print(visited)
+        visited=set()
         count=0
-        def dfs(i,j):
-            if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]):
+        lenGridX=len(grid)
+        lenGridY=len(grid[0])
+        def findadjacentones(i,j):
+            if i<0 or i>=lenGridX or j<0 or j>=lenGridY:
+                return
+            if (i,j) in visited:
                 return
             if grid[i][j]=="0":
                 return
-            grid[i][j]="0"
-            # print(visited)
-            dfs(i + 1, j)
-            dfs(i - 1, j)
-            dfs(i, j + 1)
-            dfs(i, j - 1)
+            visited.add((i,j))
+            findadjacentones(i+1,j)
+            findadjacentones(i-1,j)
+            findadjacentones(i,j+1)
+            findadjacentones(i,j-1)
             return
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if  grid[i][j]=="1":
-                    count+=1
-                    dfs(i,j)
+        for i in range(lenGridX):
+            for j in range(lenGridY):
+                if grid[i][j]=="1":
+                    if (i,j) not in visited:
+                        count+=1
+                        findadjacentones(i,j)
         return count
+
+
         
