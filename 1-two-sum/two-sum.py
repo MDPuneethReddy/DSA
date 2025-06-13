90440f61a394cf99b36f11845f6f728ff01c4ea2
MDPuneethReddy <64721445+MDPuneethReddy@users.noreply.github.com>
2025-06-13 10:59:44 -0400

    Time: 0 ms (100.00%) | Memory: 19.1 MB (24.26%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,13 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        #time complexity- 0(n), space complexity-o(n)
-        d={}
+        hashmap={}
         for i in range(len(nums)):
-            if nums[i] in d:
-                return [i,d[nums[i]]]
-            d[target-nums[i]]=i
+            dif=target-nums[i]
+            if dif in hashmap:
+                return [i,hashmap[dif]]
+            else:
+                hashmap[nums[i]]=i
         
-        #withoutspace complexity then we can sort it use left and right to find the solutiuon. the time is o(nlogn)
-
 
         
