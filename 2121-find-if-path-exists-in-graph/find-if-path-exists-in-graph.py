27a7845e6a33d9aca63780146c6972c0162264d8
MDPuneethReddy <64721445+MDPuneethReddy@users.noreply.github.com>
2025-06-17 14:31:10 -0400

    Time: 826 ms (13.08%) | Memory: 148.2 MB (5.10%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
@@ -1,57 +1,33 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        d={}
-        for i in range(len(edges)):
-            if edges[i][0] not in d:
-                d[edges[i][0]]=[edges[i][1]]
+        hashmap={}
+        for i in edges:
+            if i[0] in hashmap:
+                hashmap[i[0]].append(i[1])
             else:
-                d[edges[i][0]].append(edges[i][1])
-            if edges[i][1] not in d:
-                d[edges[i][1]]=[edges[i][0]]
+                hashmap[i[0]]=[i[1]]
+            if i[1] in hashmap:
+                hashmap[i[1]].append(i[0])
             else:
-                d[edges[i][1]].append(edges[i][0])
-        #dfs
-        visited=[0]*n
-        def recurr(entry):
-            # print(entry)
-            if entry==destination:
-                # print("yes")
-                return True
-            if visited[entry]==1:
-                return False
-            visited[entry]=1
-            found=False
-            for i in range(len(d[entry])):
-                if visited[d[entry][i]]==0:
-                    print(d[entry][i])
-                    found=found or recurr(d[entry][i])
-            return found
-        return recurr(source)
-            
+                hashmap[i[1]]=[i[0]]
 
+        visited=set()
+        self.found=False
+        def findpath(source,destination,hashmap):
+            if source==destination:
+                self.found=True
+                return 
+            if source not in hashmap:
+                return
+            visited.add(source)
+            for i in hashmap[source]:
+                if i not in visited:
+                    findpath(i,destination,hashmap)
+            return
+        findpath(source,destination,hashmap)
+        return self.found
         
 
 
-
-
-
-        # bfs
-        # queue=[source]
-        # visited=[0]*n
-        # found=False
-        # # print(d)
-        # while(queue):
-        #     v=queue.pop(0)
-        #     if v==destination:
-        #         found=True
-        #         break
-        #     if visited[v]==0:
-        #         visited[v]=1
-            
-        #         for i in range(len(d[v])):
-        #             queue.append(d[v][i])
-        #         # print(v,queue,visited)
-        # return found
-
-
+        
         
